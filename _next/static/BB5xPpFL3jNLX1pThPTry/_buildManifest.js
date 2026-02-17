d3fec356ae59003512b4da7922cdb156e6b7b1c0
SuryaVignesh06 <SuryaVignesh06@users.noreply.github.com>
2026-02-17 16:12:30 +0000

    deploy: e6de7ac7eb702b964781d870be2a98c9beb8b537

--- _next/static/BB5xPpFL3jNLX1pThPTry/_buildManifest.js
+++ _next/static/BB5xPpFL3jNLX1pThPTry/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Final_BHK//_next/:path+",
-        "destination": "/Final_BHK/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
